5839a061712f172a84f7376627bc122767e50d4e
linux5396 <929159338@qq.com>
2020-01-18 13:29:10 +0800

    日志实体和追加参数

--- src/main/java/com/qgailab/raftkv/entity/AppendLogParam.java
+++ src/main/java/com/qgailab/raftkv/entity/AppendLogParam.java
@@ -1,11 +1,163 @@
 package com.qgailab.raftkv.entity;
 
+import java.util.Arrays;
+
 /**
  * @author linxu
  * @date 2020/1/18
  * <tip>take care of yourself.everything is no in vain.</tip>
  * 追加日志参数
  */
-public class AppendLogParam extends BaseParam{
+public class AppendLogParam extends BaseParam {
+    /**
+     * 领导人的 Id，以便于跟随者重定向请求
+     */
+    private String leaderId;
 
+    /**
+     * 新的日志条目紧随之前的索引值
+     */
+    private long prevLogIndex;
+
+    /**
+     * prevLogIndex 条目的任期号
+     */
+    private long preLogTerm;
+
+    /**
+     * 准备存储的日志条目（表示心跳时为空；一次性发送多个是为了提高效率）
+     */
+    private LogEntry[] entries;
+
+    /**
+     * 领导人已经提交的日志的索引值
+     */
+    private long leaderCommit;
+
+    /**
+     * 提供给后续拓展灵活构造
+     */
+    public AppendLogParam() {
+    }
+
+    public AppendLogParam(Builder builder) {
+        setTerm(builder.term);
+        setServerId(builder.serverId);
+        setLeaderId(builder.leaderId);
+        setPrevLogIndex(builder.prevLogIndex);
+        setPreLogTerm(builder.preLogTerm);
+        setEntries(builder.entries);
+        setLeaderCommit(builder.leaderCommit);
+    }
+
+    @Override
+    public String toString() {
+        return "AppendLogParam{" +
+                "leaderId='" + leaderId + '\'' +
+                ", prevLogIndex=" + prevLogIndex +
+                ", preLogTerm=" + preLogTerm +
+                ", entries=" + Arrays.toString(entries) +
+                ", leaderCommit=" + leaderCommit +
+                ", serverId='" + serverId + '\'' +
+                ", term=" + term +
+                '}';
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+
+    public static final class Builder {
+
+        private long term;
+        private String serverId;
+        private String leaderId;
+        private long prevLogIndex;
+        private long preLogTerm;
+        private LogEntry[] entries;
+        private long leaderCommit;
+
+        private Builder() {
+        }
+
+        public Builder term(long val) {
+            term = val;
+            return this;
+        }
+
+        public Builder serverId(String val) {
+            serverId = val;
+            return this;
+        }
+
+        public Builder leaderId(String val) {
+            leaderId = val;
+            return this;
+        }
+
+        public Builder prevLogIndex(long val) {
+            prevLogIndex = val;
+            return this;
+        }
+
+        public Builder preLogTerm(long val) {
+            preLogTerm = val;
+            return this;
+        }
+
+        public Builder entries(LogEntry[] val) {
+            entries = val;
+            return this;
+        }
+
+        public Builder leaderCommit(long val) {
+            leaderCommit = val;
+            return this;
+        }
+
+        public AppendLogParam build() {
+            return new AppendLogParam(this);
+        }
+    }
+
+    public String getLeaderId() {
+        return leaderId;
+    }
+
+    public void setLeaderId(String leaderId) {
+        this.leaderId = leaderId;
+    }
+
+    public long getPrevLogIndex() {
+        return prevLogIndex;
+    }
+
+    public void setPrevLogIndex(long prevLogIndex) {
+        this.prevLogIndex = prevLogIndex;
+    }
+
+    public long getPreLogTerm() {
+        return preLogTerm;
+    }
+
+    public void setPreLogTerm(long preLogTerm) {
+        this.preLogTerm = preLogTerm;
+    }
+
+    public LogEntry[] getEntries() {
+        return entries;
+    }
+
+    public void setEntries(LogEntry[] entries) {
+        this.entries = entries;
+    }
+
+    public long getLeaderCommit() {
+        return leaderCommit;
+    }
+
+    public void setLeaderCommit(long leaderCommit) {
+        this.leaderCommit = leaderCommit;
+    }
 }
--- src/main/java/com/qgailab/raftkv/entity/LogEntry.java
+++ src/main/java/com/qgailab/raftkv/entity/LogEntry.java
@@ -1,6 +1,7 @@
 package com.qgailab.raftkv.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @author linxu
@@ -16,9 +17,124 @@ public class LogEntry implements Serializable, Comparable {
      * 日志任期号码
      */
     private long term;
+    /**
+     * 命令
+     */
+    private Command command;
+
+    public LogEntry() {
+    }
+
+    public LogEntry(long term, Command command) {
+        this.term = term;
+        this.command = command;
+    }
+
+    public LogEntry(Long index, long term, Command command) {
+        this.index = index;
+        this.term = term;
+        this.command = command;
+    }
+
+    private LogEntry(Builder builder) {
+        setIndex(builder.index);
+        setTerm(builder.term);
+        setCommand(builder.command);
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "index=" + index +
+                ", term=" + term +
+                ", command=" + command +
+                '}';
+    }
 
     @Override
     public int compareTo(Object o) {
-        return 0;
+        if (o == null) {
+            return -1;
+        }
+        if (this.getIndex() > ((LogEntry) o).getIndex()) {
+            return 1;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LogEntry logEntry = (LogEntry) o;
+        return term == logEntry.term &&
+                Objects.equals(index, logEntry.index) &&
+                Objects.equals(command, logEntry.command);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, term, command);
+    }
+
+    public static final class Builder {
+
+        private Long index;
+        private long term;
+        private Command command;
+
+        private Builder() {
+        }
+
+        public Builder index(Long val) {
+            index = val;
+            return this;
+        }
+
+        public Builder term(long val) {
+            term = val;
+            return this;
+        }
+
+        public Builder command(Command val) {
+            command = val;
+            return this;
+        }
+
+        public LogEntry build() {
+            return new LogEntry(this);
+        }
+    }
+
+    public Long getIndex() {
+        return index;
+    }
+
+    public void setIndex(Long index) {
+        this.index = index;
+    }
+
+    public long getTerm() {
+        return term;
+    }
+
+    public void setTerm(long term) {
+        this.term = term;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
     }
 }
